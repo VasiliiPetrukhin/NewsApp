a5957d8ab95c219f238abd8bae65037211029907
VasiliiPetrukhin <102147260+VasiliiPetrukhin@users.noreply.github.com>
2022-06-19 15:19:43 +0500

    commit 3

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "2115b3feb21245a99b41234068a60a25"
+    }
+}
